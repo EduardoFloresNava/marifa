d9b8f858f223531af259c7e469d992afca52a2bb
Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
2012-09-11 15:15:49 -0300

    Correciones al texto de un suceso. Faltan traducciones.

--- theme/default/views/suceso/voto_comentario_post.php
+++ theme/default/views/suceso/voto_comentario_post.php
@@ -3,7 +3,11 @@
 		<i class="icon icon-thumbs-{if="$suceso.voto"}up{else}down{/if}"></i>
 	</div>
 	<div class="contenido">
-		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">positivamente</span>{else}<span class="label label-important">negativamente</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> en el post <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a> de <a href="">{$suceso.post.usuario.nick}</a>.
+		{if="$suceso.u_voto.id === $actual.id"}
+		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente@}</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en el post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a> de <a href="">{$suceso.post.usuario.nick}</a>.
+		{else}
+		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en tu post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a>.
+		{/if}
 	</div>
 	<div class="fecha hidden-phone hidden-tablet">
 		{function="$fecha->fuzzy()"}
