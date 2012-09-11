da74c8309c31cd8e151c3e38f612f796a6e9eb38
Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
2012-09-11 20:49:01 -0300

    FIX: error en el marcado de RainTPL para traducción.

--- theme/default/views/suceso/voto_comentario_post.php
+++ theme/default/views/suceso/voto_comentario_post.php
@@ -6,7 +6,7 @@
 		{if="$suceso.u_voto.id === $actual.id"}
 		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente@}</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en el post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a> de <a href="">{$suceso.post.usuario.nick}</a>.
 		{else}
-		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en tu post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a>.
+		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente@}</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en tu post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a>.
 		{/if}
 	</div>
 	<div class="fecha hidden-phone hidden-tablet">
