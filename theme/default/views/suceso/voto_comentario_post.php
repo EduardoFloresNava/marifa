9382cb2e8c49f81999d816d1922a627c8025176a
Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
2012-09-12 20:33:19 -0300

    Cambio visualización voto comentario post.

--- theme/default/views/suceso/voto_comentario_post.php
+++ theme/default/views/suceso/voto_comentario_post.php
@@ -4,9 +4,9 @@
 	</div>
 	<div class="contenido">
 		{if="$suceso.u_voto.id === $actual.id"}
-		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente@}</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en el post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a> de <a href="">{$suceso.post.usuario.nick}</a>.
+		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">+1</span>{else}<span class="label label-important">-1</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en el post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a> de <a href="">{$suceso.post.usuario.nick}</a>.
 		{else}
-		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">{@positivamente@}</span>{else}<span class="label label-important">{@negativamente@}</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en tu post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a>.
+		<a href="/perfil/index/{$suceso.u_voto.nick}">{$suceso.u_voto.nick}</a> {@vot&oacute;@} {if="$suceso.voto"}<span class="label label-success">+1</span>{else}<span class="label label-important">-1</span>{/if} {@el comentario de@} <a href="/perfil/index/{$suceso.u_comentario.nick}">{$suceso.u_comentario.nick}</a> {@en tu post@} <a href="/post/index/{$suceso.post.id}">{$suceso.post.titulo}</a>.
 		{/if}
 	</div>
 	<div class="fecha hidden-phone hidden-tablet">
