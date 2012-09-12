41b176fc562a4928f8f7ddc4a379f704dfbabab2
Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
2012-09-11 22:20:07 -0300

    Fecha a la portada de fotos con algo de estilo.

--- theme/default/views/foto/index.php
+++ theme/default/views/foto/index.php
@@ -5,7 +5,7 @@
 				<img alt="" src="http://placehold.it/360x236">
 				<div class="caption">
 					<h4>{$value.titulo} <small>{@por@} <a href="/perfil/index/{$value.usuario.nick}">{$value.usuario.nick}</a></small></h4>
-					<p>{$value.descripcion}</p>
+					<p>{$value.descripcion|nl2br}</p>
 					<p class="toolbar">
 						<span class="btn-toolbar">
 							<span class="btn-group"><a class="btn btn-primary" href="/foto/ver/{$value.id}">{@Ver@}</a></span>
@@ -13,6 +13,7 @@
 							{if="!$value.voto"}<span class="btn-group"><a class="btn btn-success" href="/foto/votar/{$value.id}/1"><i class="icon-white icon-thumbs-up"></i></a>
 							<a class="btn btn-danger" href="/foto/votar/{$value.id}/-1"><i class="icon-white icon-thumbs-down"></i></a></span>{/if}
 						</span>
+						<span class="fecha pull-right">{function="$value.creacion->fuzzy()"}</span>
 					</p>
 				</div>
 			</div>
