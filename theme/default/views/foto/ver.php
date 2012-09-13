d2351fc94a72fa4a0fff5d1be3ad27af3c1eabd3
Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
2012-09-13 06:44:26 -0300

    Agregadas traducciones a las fotos.

--- theme/default/views/foto/ver.php
+++ theme/default/views/foto/ver.php
@@ -1,20 +1,20 @@
 <div class="row">
 	<div class="span2">
-		<h3>Posteado por:</h3>
+		<h3>{@Autor@}:</h3>
 		<img class="thumbnail" src="{function="Utils::get_gravatar($usuario.email, 160, 160)"}" />
 		<h4>{$usuario.nick}</h4>
 		{if="$me != NULL && $me != $usuario.id"}<div class="row-fluid">
-			<a href="#" class="btn span12" style="min-height: 0;">Seguir usuario</a>
+			<a href="#" class="btn span12" style="min-height: 0;">{@Seguir usuario@}</a>
 		</div>{/if}
-		<p><strong>Seguidores:</strong> {$usuario.seguidores}</p>
-		<p><strong>Puntos:</strong> {$usuario.puntos}</p>
-		<p><strong>Posts:</strong> {$usuario.posts}</p>
-		<p><strong>Comentarios:</strong> {$usuario.comentarios}</p>
+		<p><strong>{@Seguidores@}:</strong> {$usuario.seguidores}</p>
+		<p><strong>{@Puntos@}:</strong> {$usuario.puntos}</p>
+		<p><strong>{@Posts@}:</strong> {$usuario.posts}</p>
+		<p><strong>{@Comentarios@}:</strong> {$usuario.comentarios}</p>
 	</div>
 	<div class="span10">
 		{if="isset($success)"}
 		<div class="alert alert-success">
-			<strong>&iexcl;Felicidades!</strong> {$success}
+			<strong>{@&iexcl;Felicidades!@}</strong> {$success}
 		</div>{/if}
 		<!--
 		<div class="row-fluid">
@@ -29,20 +29,20 @@
 		<div class="btn-toolbar">
 			<div class="btn-group">
 				{if="!$ya_vote"}<a href="/foto/votar/{$foto.id}/1" class="btn btn-success"><i class="icon-white icon-thumbs-up"></i></a>{/if}
-				<span href="#" class="btn{if="$foto.votos != 0"} {if="$foto.votos < 0"}btn-danger{else}btn-success{/if}{/if}">Votos: {$foto.votos}</span>
+				<span href="#" class="btn{if="$foto.votos != 0"} {if="$foto.votos < 0"}btn-danger{else}btn-success{/if}{/if}">{@Votos@}: {$foto.votos}</span>
 				{if="!$ya_vote"}<a href="/foto/votar/{$foto.id}/-1" class="btn btn-danger"><i class="icon-white icon-thumbs-down"></i></a>{/if}
 			</div>
 			<div class="btn-group">
-				{if="!$es_favorito"}<a href="/foto/favorito/{$foto.id}" class="btn btn-success"><i class="icon-white icon-star"></i> Agregar a favoritos</a>{/if}
-				<span href="#" class="btn">Favoritos: {$foto.favoritos}</span>
+				{if="!$es_favorito"}<a href="/foto/favorito/{$foto.id}" class="btn btn-success"><i class="icon-white icon-star"></i> {@Agregar a favoritos@}</a>{/if}
+				<span href="#" class="btn">{@Favoritos@}: {$foto.favoritos}</span>
 			</div>
 			<div class="btn-group">
-				<span href="#" class="btn">Visitas: {$foto.visitas}</span>
-				{if="$foto.visitas > 0"}<span href="#" class="btn">&Uacute;ltima visita: {function="$foto.ultima_visita->format('d/m/Y H:i:s')"}</span>{/if}
+				<span href="#" class="btn">{@Visitas@}: {$foto.visitas}</span>
+				{if="$foto.visitas > 0"}<span href="#" class="btn">{@&Uacute;ltima visita@}: {$foto.ultima_visita->fuzzy()}</span>{/if}
 			</div>
 			{if="$me != NULL && $foto.usuario_id != $me"}
 			<div class="btn-group pull-right">
-				<a href="#" class="btn btn-danger">Denunciar</a>
+				<a href="#" class="btn btn-danger">{@Denunciar@}</a>
 			</div>
 			{/if}
 		</div>
@@ -57,7 +57,7 @@
 						<div class="clearfix">
 							<span>
 								<a href="#">{$value.usuario.nick}</a>
-								<small>{function="$value.fecha->format('d/m/Y H:i:s')"}</small>
+								<small>{$value.fecha->fuzzy()}</small>
 							</span>
 							<div class="btn-group pull-right">
 								{if="$me != NULL"}<a href="#" class="btn btn-mini btn"><i class="icon icon-comment"></i></a>{/if}
@@ -70,7 +70,7 @@
 				</div>
 				{else}
 				<div class="alert">
-					No hay comentarios sobre esta foto hasta el momento.
+					{@No hay comentarios sobre esta foto hasta el momento.@}
 				</div>
 				{/loop}
 			</div>
@@ -79,17 +79,17 @@
 		<form method="POST" action="/foto/comentar/{$foto.id}">
 			{if="isset($comentario_success)"}
 			<div class="alert alert-success">
-				<strong>&iexcl;Felicidades!</strong> {$comentario_success}
+				<strong>{@&iexcl;Felicidades!@}</strong> {$comentario_success}
 			</div>{/if}
 			{if="isset($comentario_error)"}
 			<div class="alert">
-				<strong>&iexcl;Error!</strong> {$comentario_error}
+				<strong>{@&iexcl;Error!@}</strong> {$comentario_error}
 			</div>{/if}
 			<textarea class="span10" name="comentario" value="comentario">{$comentario_content}</textarea>
-			<button type="submit" class="btn btn-primary">Comentar</button>
+			<button type="submit" class="btn btn-primary">{@Comentar@}</button>
 		</form>
 		{else}
-		<div class="alert">Para poder comentar debes ingresar.</div>
+		<div class="alert">{@Para poder comentar debes estar registrado.@}</div>
 		{/if}
 	</div>
 </div>
