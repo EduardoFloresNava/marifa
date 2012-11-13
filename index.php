7bde5738d2b3349001aacfb4db1ce789a0f6ea46
Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
2012-11-13 12:26:36 -0300

    Agregada constante con version de marifa.

--- index.php
+++ index.php
@@ -38,6 +38,11 @@ else
 	// PARA PRODUCCION DEBE SER FALSE.
 }
 
+/**
+ * Defino versión de marifa.
+ */
+define('VERSION', '0.1');
+
 // Suprimimos advertencias de DateTime. Si lo deseas puedes poner una TZ estatica.
 date_default_timezone_set('UTC');
 
